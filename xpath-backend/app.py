08e0714e07ab668571510de1cf00c3cf16181218
Gopal Krishna <gopal327.be22@chitkara.edu.in>
2025-06-08 21:15:33 +0530

    change

--- xpath-backend/app.py
+++ xpath-backend/app.py
@@ -71,4 +71,5 @@ def get_xpaths():
     return jsonify({"xpaths": stored_xpaths[url]}), 200
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
